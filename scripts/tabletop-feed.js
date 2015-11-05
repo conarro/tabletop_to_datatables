9337dac3d918b7b552d0418784a0d64d25b19b83
Kyle Conarro <kyle.conarro@gmail.com>
2015-11-05 01:15:34 -0500

    updates

--- scripts/tabletop-feed.js
+++ scripts/tabletop-feed.js
@@ -1,10 +1,10 @@
 var jqueryNoConflict = jQuery;
-
+var CSH = {};
 // begin main function
 jqueryNoConflict(document).ready(function(){
 
-    initializeTabletopObject("https://docs.google.com/spreadsheets/d/1WapMdzj1DJWuBOBN1TLrtxTVXak2KjdixGVxHjcRp9s/pubhtml");
-
+    initializeTabletopObject("https://docs.google.com/spreadsheets/d/1mHcnstovekJOceCtlcESnKaWfkbrMoChVPu8hgF4_f4/pubhtml?gid=22972212&single=true");
+    $("#table").hide();
 });
 
 // pull data from google spreadsheet
@@ -17,54 +17,54 @@ function initializeTabletopObject(dataSpreadsheet){
     });
 }
 
-// create table headers
-function createTableColumns(){
+var tableColumns = [
+  {"mDataProp": "advice", "sTitle": "Advice", "sClass": "center"},
+  {"mDataProp": "namescrub", "sTitle": "Name", "sClass": "center", "hasFilter": true},
+  {"mDataProp": "typeofadvice", "sTitle": "Category", "sClass": "center", "hasFilter": true},
+  {"mDataProp": "authororiginscrub", "sTitle": "Author Origin", "sClass": "center", "hasFilter": true},
+  {"mDataProp": "standlocation", "sTitle": "Stand Location", "sClass": "center", "hasFilter": true},
+  {"mDataProp": "date", "sTitle": "Date", "sClass": "center"},
+  {"mDataProp": "region", "sTitle": "Region", "sClass": "center", "hasFilter": true},
+  {"mDataProp": "imageurl", "sTitle": "Image URL", "sClass": "center"}
+];
 
-    /* swap out the properties of mDataProp & sTitle to reflect
-    the names of columns or keys you want to display.
-    Remember, tabletop.js strips out spaces from column titles, which
-    is what happens with the More Info column header */
+var filterableColumns = $.grep(tableColumns, function(value, index) {
+  return value.hasFilter;
+});
 
-    var tableColumns =   [
-        {"mDataProp": "gender", "sTitle": "Gender", "sClass": "center"},
-        {"mDataProp": "age", "sTitle": "Age", "sClass": "center"},
-        {"mDataProp": "country", "sTitle": "Country", "sClass": "center"},
-        {"mDataProp": "advice", "sTitle": "Advice", "sClass": "center"},
-        {"mDataProp": "place", "sTitle": "Advice", "sClass": "center"},
-        {"mDataProp": "locationscrub", "sTitle": "Location Scrub", "sClass": "center"},
-        {"mDataProp": "place", "sTitle": "Place", "sClass": "center"},
-        {"mDataProp": "typeofadvice", "sTitle": "Type of Advice", "sClass": "center"},
-        {"mDataProp": "categoryofevent", "sTitle": "Category of Event", "sClass": "center"},
-        {"mDataProp": "agebucket", "sTitle": "Age Bucket", "sClass": "center"},
-        {"mDataProp": "agegroup", "sTitle": "Age Group", "sClass": "center"},
-        {"mDataProp": "date", "sTitle": "Date", "sClass": "center"},
-        {"mDataProp": "city", "sTitle": "City", "sClass": "center"},
-        {"mDataProp": "stateprovince", "sTitle": "State / Province", "sClass": "center"},
-        {"mDataProp": "country", "sTitle": "Country", "sClass": "center"},
-        {"mDataProp": "name", "sTitle": "Name", "sClass": "center"},
-        {"mDataProp": "gender", "sTitle": "Gender", "sClass": "center"}
-    ];
-    return tableColumns;
-}
+var filterableColumnNames = $.map(filterableColumns, function(value, index) {
+  return value.sTitle;
+});
+
+var filterableColumnIds = $.map(filterableColumns, function(value, index) {
+  return value.mDataProp;
+});
 
 // create the table container and object
 function writeTableWith(dataSource){
 
-    jqueryNoConflict("#demo").html("<table cellpadding='0' cellspacing='0' border='0' class='display table table-bordered table-striped' id='data-table-container'></table>");
+  jqueryNoConflict("#table").html("<table cellpadding='0' cellspacing='0' border='0' class='display table table-bordered table-striped' id='datatable'></table>");
 
-    var oTable = jqueryNoConflict("#data-table-container").DataTable({
-        "sPaginationType": "bootstrap",
-        "iDisplayLength": 25,
-        "aaData": dataSource,
-        "aoColumns": createTableColumns(),
-        "oLanguage": {
-            "sLengthMenu": "_MENU_ records per page"
-        },
-        initComplete: function () {
-          this.api().columns().every( function () {
-            var column = this;
-            var select = $('<select><option value=""></option></select>')
-              .appendTo( $(column.header()) )
+  var table = jqueryNoConflict("#datatable").DataTable({
+      "sPaginationType": "bootstrap",
+      "iDisplayLength": 10,
+      "aaData": dataSource,
+      "aoColumns": tableColumns,
+      "oLanguage": {
+          "sLengthMenu": "_MENU_ records per page"
+      },
+      initComplete: function () {
+
+        this.api().columns().every( function () {
+          var column = this;
+          var columnName = $(column.header()).text();
+
+          var filterIndex = $.inArray(columnName, filterableColumnNames);
+
+          if (filterIndex > -1) {
+            var selectId = filterableColumns[filterIndex].mDataProp;
+            var select = $('<select class="advice-filter" id="' + selectId + '" style="width:150px;"><option value="">' + columnName + '</option></select>')
+              .appendTo( $("#filters") )
               .on( 'change', function () {
                 var val = $.fn.dataTable.util.escapeRegex(
                   $(this).val()
@@ -77,11 +77,41 @@ function writeTableWith(dataSource){
 
             column.data().unique().sort().each( function ( d, j ) {
               select.append( '<option value="'+d+'">'+d+'</option>' )
-            } );
-          } );
-        }
-    });
+            });
+          }
+        });
+      },
+      "drawCallback": function( settings ) {
+          var api = this.api();
+          // if no filters are set, only show the current page of data
+          // otherwise show the filtered data
+          var rowData;
 
+          var hasActiveFilters = $("#filters select").filter(function(value, index) {
+            return $(this).val().length > 0;
+          }).length > 0;
+
+          rowData = hasActiveFilters ? api.rows({search:'applied'}).data() : api.rows({page: 'current'}).data();
+          debugger;
+          var imageRows = $.grep(rowData, function(value, index) {
+             return value.imageurl;
+          });
+
+          var images = $.map(imageRows, function(value, index) {
+            return "<img src='" + value.imageurl + "' />";
+          });
+
+          var $results = $("#results");
+          if (images.length > 0) {
+            $results.html(images.join('<br>'));
+          } else {
+            $results.html('No matches');
+          }
+
+      }
+  });
+
+  randomizeFilters()
 };
 
 //define two custom functions (asc and desc) for string sorting
@@ -92,3 +122,28 @@ jQuery.fn.dataTableExt.oSort["string-case-asc"]  = function(x,y) {
 jQuery.fn.dataTableExt.oSort["string-case-desc"] = function(x,y) {
     return ((x < y) ?  1 : ((x > y) ? -1 : 0));
 };
+
+function randomizeFilters() {
+  // select a random table row
+  // set the select boxes to the random row values
+  //
+  var rowData = $("#datatable").dataTable().api().rows().data();
+  var randomRow = rowData[Math.floor(Math.random() * rowData.length)];
+
+  $.each(randomRow, function(key, value) {
+    var filterIndex = $.inArray(key, filterableColumnIds);
+    if (filterIndex > -1) {
+      var $filter = $('#' + filterableColumns[filterIndex].mDataProp);
+      $filter.val(value);
+      $filter.trigger('change');
+    }
+
+  });
+}
+
+function resetFilters() {
+  $("#filters select").each(function() {
+    $(this).val("");
+    $(this).trigger('change');
+  });
+}
